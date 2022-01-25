ec7b2856dc849d63dfc3a45342ac631539231e41
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-01-25 12:10:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let container = document.querySelector(".buttons");
+
+function add(){
+  let button = document.createElement("button");
+  let buttonText = document.createTextNode("submit");
+  button.appendChild(buttonText); 
+  container.appendChild(button);
+  button.onclick = add;
+}
+
 function rmv(){
   Array.from(document.querySelectorAll('button')).forEach(function (button) {
 	button.remove();
