1c405d727df402416ae906fdc8bda64c30c685a5
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-01-27 12:09:04 -0500

    Update buttonSelector.js

--- buttonSelector.js
+++ buttonSelector.js
@@ -1,19 +1,20 @@
 let container = document.querySelector(".buttons");
 let attacks = ["strike", "strongStrike","normalAttack"]
 
-
+//this starts up the attack selectors
 function attackMenu(event){
   removeButton();
   for(let i=0; i<attacks.length; i++){
   var button = document.createElement("button");
   var buttonText = document.createTextNode(attacks[i]);
   button.appendChild(buttonText); 
-  button.id=i;
+  button.id = i;
   container.appendChild(button);
   document.getElementById(i).addEventListener("click", runAttack);
   }
 }
 
+//this function runs the attack you chose
 function runAttack(event){
   let attack = parseInt(event.target.id,36);
   alert(attack + 1);
