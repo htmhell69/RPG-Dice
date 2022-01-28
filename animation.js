47116c25454836a99c9e473a06e4503d12f90e19
Jack Shea <jack20shea20@gmail.com>
2022-01-28 15:34:08 -0500

    made it so the images scale with the canvas

--- animation.js
+++ animation.js
@@ -1,8 +1,10 @@
 setInterval(resizingCanvas, 10);
 function resizingCanvas(){
+   let width = (window.innerWidth/3 + window.innerHeight/3)/2;
+   let height = (window.innerWidth/2 + window.innerHeight/2)/2;
    canvas.height = window.innerHeight/1.5;
    canvas.width = window.innerWidth;
-   
-    
+   ctx.drawImage(enemyImage, window.innerWidth-window.innerWidth/3.5 - 10, 10, width, height);
+   ctx.drawImage(playerImage, 10, 10, width, height);
     
 }
