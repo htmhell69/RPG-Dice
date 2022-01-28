13f114cf63af35dc2485863db1c69cb7898c4461
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-01-28 08:07:44 -0500

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,8 +1,9 @@
 setInterval(10, resizingCanvas)
 function resizingCanvas(){
-    
+    alert("i ran")
     canvas.height = window.innerHeight/1.5;
     canvas.width = window.innerWidth;
+    
     
     
 }
