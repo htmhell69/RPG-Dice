448ab3599e17d0f8186e05016c9e5f0b9d58e19e
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-02-03 07:28:51 -0500

    Update saveData.js

--- saveData.js
+++ saveData.js
@@ -1,5 +1,13 @@
-if (typeof(Storage) !== "undefined") {
-    // Code for localStorage/sessionStorage.
-  } else {
-    alert("We are sorry for the inconvenience your browser will not allow us to save your game");
-  }
+
+
+
+function(){
+    
+    if (typeof(Storage) !== "undefined") {
+        localStorage = setItem("turns", turnOrder);
+        alert(localStorage.getItem("turns"))
+    } else {
+        alert("We are sorry for the inconvenience your browser will not allow us to save your game");
+    }
+
+}
