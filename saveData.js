7859b2e58e0a086389f1e115f3ff1cd03ed6d07c
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-02-04 10:40:19 -0500

    Update saveData.js

--- saveData.js
+++ saveData.js
@@ -14,8 +14,8 @@ function readSaveData(){
     if(localStorage.getItem("entities") == null){
         alert("you do not have any save data press the save button in the left hand corner of your screen to save");
     } else{
-        alert((localStorage.getItem("entities")));
-        
+        alert(localStorage.getItem("entities"));
+        turnOrder = localStorage.getItem("entities").parse;
 
         startMenu();
     }
