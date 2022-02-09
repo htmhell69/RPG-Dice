a08e85387cc3c02041d67abc36371b7fd086f162
Jack Shea <jack20shea20@gmail.com>
2022-02-09 13:14:57 -0500

    added descriptions in the middle rectangle

---
+++ clientSide/descriptions.js
@@ -0,0 +1,49 @@
+let div = $(".description");
+
+function description(event) {
+  div.empty();
+  let id = event.target.id;
+  console.log(id);
+  switch (id) {
+    case "load":
+      div.append(
+        "<h1 font-size: 150%>Load Save:</h1>" +
+          "<p> Clicking on this will start a game with the data you have saved to save data simply press Save Game in the left corner of your screen <p>"
+      );
+      break;
+    case "new":
+      div.append(
+        "<h1 font-size: 150%>New Game:</h1>" +
+          "<p> Clicking on this will start a fresh game doing so will not override your save data<p>"
+      );
+      break;
+    case "shop":
+      div.append(
+        "<h1 font-size: 150%>Shop:</h1>" +
+          "<p> The shop is where you would go if you want to buy weapons items and multiple other things now it is not finished but will be in a later update. <p>"
+      );
+      break;
+    case "attack":
+      div.append(
+        "<h1 font-size: 150%>Attack:</h1>" +
+          "<p> Clicking this will allow you to choose a weapon to strike with.<p>"
+      );
+      break;
+    case "back":
+      div.append(
+        "<h1 font-size: 150%>Back:</h1>" +
+          "<p> Clicking this will send you back to the menu you were last at<p>"
+      );
+      break;
+    default:
+      if (!isNaN(id)) {
+        div.append(
+          "<p><img src=" +
+            turnOrder[currentTurn].weapons[id].imgSrc +
+            "><img>" +
+            turnOrder[currentTurn].weapons[id].description +
+            "</p>"
+        );
+      }
+  }
+}
